94588793d261687cbc5eb4332485b9a59d41e311
Alberto Neculhueque <aang1999cl@gmail.com>
2024-03-13 10:48:13 -0300

    operaciones y propiedades con str

---
+++ Dia 2/ejercicio1.py
@@ -0,0 +1,17 @@
+
+#Dia 2 causa
+edad="24" #str
+numero=100 #num 242424
+#duplacacion
+print(3*edad) #se repite 3 veces edad (no opera por que es un caracter)
+print (3*numero) #opera por que es numero
+print(type(edad) )  #<class 'str'>
+print(type(numero) ) # <class 'int'>
+
+#metodo count()
+print("Mapache, dime donde estas Mapache che".count("Mapache")) #esta contenido dos veces dentro de la oracion
+print("Mapache, dime donde estas Mapache che".count("che")) #che esta dentro de mapa(che)x2 y el che serian 3
+
+#metodo upper ()mayuscula  y lower()minuscula 
+print("Soy malisIMo".upper()) #imprime la palabra en mayuscula "SOY MALISIMO"
+print("BuEnOs DiAs" .lower()) #imprime la palabra en minuscula "buenos dias"
