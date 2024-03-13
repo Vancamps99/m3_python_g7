d3dd34eab0d0f3e2d1b92b0d8906dde746fa0bbf
Alberto Neculhueque <aang1999cl@gmail.com>
2024-03-13 12:58:27 -0300

    operadores adicionales y librerias

---
+++ Dia 2/ejercicio2_operador.py
@@ -0,0 +1,12 @@
+
+import math #libreria que añade "funcionalidad al codigo"
+
+print(2**3) # "2" elevado a 3  "8"
+print(5/2) # 5/2  con resultado flotante "2.5"
+print(5//2) # 5/2  pero solo muestra la parte entera  "2"
+print(5%2) #modulo o resto de la division "1"
+#hay un orden de operacion ojo con el parentesis
+
+print(math.pow(4,4)) #4**4
+print(math.sqrt(64)) #root of 64 equals 8
+print(math.ceil(3.4))
