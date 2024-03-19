85f3444ab185387dbde49c997e1b3093dd9ab1b4
Alberto Neculhueque <aang1999cl@gmail.com>
2024-03-19 12:12:03 -0300

    añadiendo parte 2 del desafio

--- Dia 6/Desafio/cachipum.py
+++ Dia 6/Desafio/cachipum.py
@@ -1,9 +1,13 @@
 import random
 import sys
+
 print("Vamos a jugar al cachipun para ello deberas elejir una de las siguientes opciones Piedra, Papel o Tijeras")
+
 opciones = ['Piedra', 'Papel','Tijeras']
 jugador1 = input(f"Elige {opciones} \n")
+
 computador = random.choice(opciones)
+
 if jugador1 == "Piedra" :
     print(f"Tu eleccion es {jugador1}")
 elif jugador1 == "Papel" :
@@ -28,5 +32,3 @@ elif jugador1 == "Tijeras" and computador == "Papel" :
     print(f"Elejiste {jugador1} y la computadora eligio {computador}, Ganaste")
 else :
     print(f"Ambos elijieron la opción {jugador1}, es un Empate")
-
-#desafio
