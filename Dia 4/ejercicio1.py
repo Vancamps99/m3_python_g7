e4168f9dfe0b232d7e9a177e059d65273cc6866a
Alberto Neculhueque <aang1999cl@gmail.com>
2024-03-19 11:55:25 -0300

    añadiendo carpetas

---
+++ Dia 4/ejercicio1.py
@@ -0,0 +1,7 @@
+#escriba dos numeros y el resultado de ella
+
+numero1 = int(input("ingrese el primer numero "))
+numero2 = int(input("ingrese el segundo numero "))
+suma=numero1+numero2
+print("la suma de los dos numeros es ",suma)
+
---
+++ Dia 4/ejercicio2.py
@@ -0,0 +1,8 @@
+#cambio de rueda
+#sacar el auto del trafico,colocarse chaleco ,luces intermitentes
+#verificar que tan dañado esta
+#verificar si pose herramientas y ruedas de repuesto
+#en caso de que si "aflojar tuercas" ,utilizar la gata hidraulico, retirar la rueda,colocar rueda,levantar auto , apretar la rueda inspeccion
+#en caso de que no llamar a mecanico o grua
+cuales son los pasos a realizar para llegar a su trabajo o universidad
+
