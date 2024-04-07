9aef7a17aeb056ce23989855439991c3f3954f44
Vancamps99 <aang1999cl@gmail.com>
2024-04-06 22:25:12 -0300

    Que paso

--- Dia 18/main.py
+++ Dia 18/main.py
@@ -8,10 +8,9 @@ def lista_a_mostrar():
         aves_info=aves_template.substitute(nom_esp=ave["name"]["spanish"],nom_ing=ave["name"]["english"] ,img=ave["images"]["main"])
         lista_vacia+=aves_info #pasar info procesada #+= añade hasta el 205
     html=html_template.substitute(body=lista_vacia)
-    with open('aves.html','w' ) as f:
+    with open('aves.html','w', encoding='utf-8' ) as f: #Encoding para mostrar caracteres especiales
         f.write(html)
 
 lista_a_mostrar()
         
-    
-     
+#Disculpe profe si ve esto. aún me cuesta . voy a tratar de ejercitar .
